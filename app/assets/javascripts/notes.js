e3eaaa84e8bb3dd35dff06e27be7d43f368de6b5
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-11 18:47:23 +0000

    adding js-next to trip show

---
+++ app/assets/javascripts/notes.js
@@ -0,0 +1,46 @@
+//$(document).ready(function(){
+//  attachListeners()
+//})
+
+//function moreInfo(element){}
+//function hideInfo(element){}
+//function nextTrip(){}
+
+//Sift Through Trips
+$(function() {
+  $('.js-next').on('click', function() {
+    const currentId = parseInt($(".js-next").attr("data-id"))
+    const nextId = currentId + 1;
+    $.get("/trips/" + nextId + ".json", function(tripJSON) {
+      const trip = new Trip(tripJSON);
+      trip.renderNext()
+    })
+
+    //empty previous trip's items, old + new
+    $(".items").html("")
+    $(".newItem").html("")
+
+    // change attribute to sift to next trip
+    $(".js-next").attr("data-id", nextId)
+
+    // change href of tripName
+    $(".tripName").attr("href", `/trip/${nextId}`)
+
+    // change href of "Edit Trip"
+    const newHref = $(".edit_trip").attr("href").replace(currentId, nextId)
+    $(".edit_trip").attr("href", newHref)
+  })
+})
+
+// Prototype methods
+Trip.prototype.formatLink = function() {
+  let tripHTML = `<a href="/trips/${this.id}" data-id=${this.id}>${this.name}</a><br>`
+  return tripHTML
+}
+
+Trip.prototype.renderNext = function() {
+	$(".tripName").text(this.name)
+	$(".tripCategories").text(this.categories)
+	$(".tripContent").text(this.content)
+	$(".tripUser").text(this.user.name)
+}
