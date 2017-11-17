0af7d5ca08ea054126471e9cee889158479e3345
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-17 00:46:38 +0000

    refactoring with instructor

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,26 @@
 // Show User Trips
 function showTrips(element){
   var id = element.dataset.id
-  $.get('/users/' + id + '/trips' +'.json', function(data){
-    var trips = data
-    var infoList = ""
-    for (var i = 0; i < trips.length; i++){
-      let tripLink = "/trips/trip[i]['id']"
-      infoList += "<li>" + "<h4>" + "Trip:" + " " + "<a href='/trips/" + trips[i]["id"] + "'>" + trips[i]["name"] + "</a>" + "</h4>" + " " +
-      "<b>" + " Categories:" + "</b>" + " " + trips[i]["categories"][0]["name"] + ", " + trips[i]["categories"][1]["name"] + "<br>" +
-      "<b>" + " Content:" + "</b>" + " " + trips[i]["content"] + " " + "</li>"
-    }
+  $.get('/users/' + id + '/trips' +'.json', function(trips) {
+
+    var infoList = trips
+                    .sort((a, b) => a.name.toUpperCase() > b.name.toUpperCase())
+                    .map(attributes => new Trip(attributes).renderListItem())
+                    .join('')
     $("#userShow-" +id).html(infoList)
   })
   $("#more-" + id + "-user").replaceWith(`<button id="hide-${id}-user" class="js-hide" data-id="${id}" onClick= hideInfo(this)> Hide Trips</button>`)
+}
+
+Trip.prototype.renderListItem = function() {
+  return (`
+    <li>
+      <h4>Trip: <a href='/trips/${this.id}'> ${this.name} </a> </h4>
+      <b>Categories:</b> ${this.categories[0].name}, ${this.categories[1].name}
+      <br>
+      <b>Content:</b> ${this.content}
+    </li>
+  `)
 }
 
 // Hide Trips
